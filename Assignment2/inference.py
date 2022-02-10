f97f96bb3b224d6e53271de9ae3f6669a756d3d2
Jonathan Chen <jchen1199@gmail.com>
2022-02-10 15:53:33 -0500

    Tokenize text before inference

--- Assignment2/inference.py
+++ Assignment2/inference.py
@@ -5,16 +5,27 @@
 import pickle
 import re
 import sys
+from nltk.corpus import stopwords
+import nltk.tokenize as tok
 
-def read_files(file_name):
+def read_files(file_name, model):
     # Read data from files into a list
-    lines = []
+    original_lines = []
+    tokenized_lines = []
+    stop_words = set(stopwords.words('english'))
+
     with open(file_name) as file:
         for line in file:
-            string = re.sub("[\"\n]", "", line)
-            lines.append(string)
+            tokens = tok.word_tokenize(line)
+
+            if 'ns' in model:
+                tokens = [x for x in tokens if x.lower() not in stop_words]
+
+            string = ", ".join(tokens)
+            original_lines.append(re.sub("[\"\n]", "", line))
+            tokenized_lines.append(string)
     
-    return lines
+    return original_lines, tokenized_lines
 
 def main(text_file, model):
     # Assumes the pickle files are in the data/ folder of the directory
@@ -22,11 +33,13 @@ def main(text_file, model):
 
     text_vector, classifier = pickle.load(model_pkl)
 
-    text = read_files(text_file)
-    transformed_text = text_vector.transform(text)
+    original_text, tokenized_text = read_files(text_file, model)
+
+    transformed_text = text_vector.transform(tokenized_text)
+
     pred_labels = classifier.predict(transformed_text)
 
-    for i, sentence in enumerate(text):
+    for i, sentence in enumerate(original_text):
         print(f"SENTENCE: {sentence}\t LABEL: {pred_labels[i]}")
  
 
